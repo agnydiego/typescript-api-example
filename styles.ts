7a8dbf7d6ec189461056f7f5cd4bbf346526202b
Agny Silva <agny.silva@solides.com.br>
2024-11-07 00:48:31 -0300

    correção commit

--- styles.ts
+++
@@ -1,6 +0,0 @@
-export const container = "bg-gray-200 min-h-screen";
-export const header = "bg-gray-100 py-4 flex justify-center items-center";
-export const headerContent = "flex items-center space-x-4";
-export const logo = "h-20 animate-spin-slow";
-export const link = "ml-4 text-blue-500 hover:text-blue-700 font-semibold text-lg";
-export const mainContent = "p-4";
